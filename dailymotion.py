722098a2c90883a270d11127440663cf6b27de1f
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-08-26 09:14:03 +0200

    Add files via upload
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

---
+++ dailymotion.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+def get_dailymotion_streams(video_id: str):
+    """
+    Retrieves Dailymotion streams based on the video ID.
+
+    Args:
+        video_id (str): The ID of the Dailymotion video.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If an error occurs while retrieving the streams.
+    """
+    try:
+        url = f'https://www.dailymotion.com/player/metadata/video/{video_id}'
+        response = requests.get(url).json()
+        stream_url = response['qualities']['auto'][0]['url']
+        m3u = requests.get(stream_url).text
+        print(m3u)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python dailymotion.py stream")
+    else:
+        video_id = sys.argv[1]
+        get_dailymotion_streams(video_id)
