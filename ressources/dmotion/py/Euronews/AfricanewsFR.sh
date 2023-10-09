92693da4d7f39f09c34ae99c86692538b4c4fdd7
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 18:40:59 +0200

    Update AfricanewsFR.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Euronews/AfricanewsFR.sh
+++ ressources/dmotion/py/Euronews/AfricanewsFR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/Euronews/EuronewsFR.py > ressources/dmotion/py/Euronews/AfricanewsFR.m3u8
+python3 ressources/dmotion/py/Euronews/AfricanewsFR.py > ressources/dmotion/py/Euronews/AfricanewsFR.m3u8
 
 exit 0
