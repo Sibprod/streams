19ef92130694fd9a2f82c1dba49987a75f55b828
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 12:10:22 +0200

    Update EuronewsFR.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Euronews/EuronewsFR.sh
+++ ressources/dmotion/py/Euronews/EuronewsFR.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/EuronewsFR/EuronewsFR.py > ressources/dmotion/py/Euronews/EuronewsFR.m3u8
+python3 ressources/dmotion/py/Euronews/EuronewsFR.py > ressources/dmotion/py/Euronews/EuronewsFR.m3u8
 
 exit 0
