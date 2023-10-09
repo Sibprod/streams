582481a68a25107339c9b9b9de3f1369c432ae9d
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 18:20:35 +0200

    Update and rename Lyon Capitale.sh to LyonCapitale.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/SudRadio/Lyon Capitale.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/dmotion/py/SudRadio/Lyon Capitale.py > ressources/dmotion/py/SudRadio/Lyon Capitale.m3u8
-
-exit 0
---
+++ ressources/dmotion/py/SudRadio/LyonCapitale.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ressources/dmotion/py/SudRadio/LyonCapitale.py > ressources/dmotion/py/SudRadio/LyonCapitale.m3u8
+
+exit 0
