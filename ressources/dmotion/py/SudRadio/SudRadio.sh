ad11f7f55c64c644ea6ac9969856d23cbb897bbd
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 11:34:07 +0200

    Update SudRadio.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/SudRadio/SudRadio.sh
+++ ressources/dmotion/py/SudRadio/SudRadio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/E1/E1.py > ressources/dmotion/py/SudRadio/SudRadio.m3u8
+python3 ressources/dmotion/py/SudRadio/SudRadio.py > ressources/dmotion/py/SudRadio/SudRadio.m3u8
 
 exit 0
