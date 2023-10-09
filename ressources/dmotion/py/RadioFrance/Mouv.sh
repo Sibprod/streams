b314ab61da6fb5575202722232f50775d2096266
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 12:46:16 +0200

    Update Mouv.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/RadioFrance/Mouv.sh
+++ ressources/dmotion/py/RadioFrance/Mouv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/RTL2/RTL2.py > ressources/dmotion/py/RadioFrance/Mouv.m3u8
+python3 ressources/dmotion/py/RadioFrance/Mouv.py > ressources/dmotion/py/RadioFrance/Mouv.m3u8
 
 exit 0
