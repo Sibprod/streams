3b0c44e31ca8cbc1449293568906b5e9ad33b885
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 14:42:17 +0200

    Update APlusCI.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Canal/APlusCI.sh
+++ ressources/dmotion/py/Canal/APlusCI.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/E1/E1.py > ressources/dmotion/py/Canal/APlus.m3u8
+python3 ressources/dmotion/py/Canal/APlusCI.py > ressources/dmotion/py/Canal/APlus.m3u8
 
 exit 0
