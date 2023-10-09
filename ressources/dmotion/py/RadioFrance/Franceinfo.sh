704d2734b4ba729a02a5e90a579e16d5441a5451
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-09 13:04:05 +0200

    Update Franceinfo.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/RadioFrance/Franceinfo.sh
+++ ressources/dmotion/py/RadioFrance/Franceinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/E1/E1.py > ressources/dmotion/py/RadioFrance/Franceinfo.m3u8
+python3 ressources/dmotion/py/RadioFrance/Franceinfo.py > ressources/dmotion/py/RadioFrance/Franceinfo.m3u8
 
 exit 0
