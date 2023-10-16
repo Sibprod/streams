4f5f5250fa145829cc57f19a15179cab94b97117
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-16 20:38:36 +0200

    Update and rename 8MB.sh to Telenantes.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/8MB/8MB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/dmotion/py/8MB/8MB.py > ressources/dmotion/py/8MB/8MB.m3u8
-
-exit 0
---
+++ ressources/dmotion/py/Telenantes/Telenantes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ressources/dmotion/py/Telenantes/Telenantes.py > ressources/dmotion/py/Telenantes/Telenantes.m3u8
+
+exit 0
