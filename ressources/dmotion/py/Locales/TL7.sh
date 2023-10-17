34d9d0edfefce469e4636f90c10153eb088f6347
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-17 19:54:54 +0200

    Update TL7.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Locales/TL7.sh
+++ ressources/dmotion/py/Locales/TL7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/TL7/TL7.py > ressources/dmotion/py/TL7/TL7.m3u8
+python3 ressources/dmotion/py/Locales/TL7.py > ressources/dmotion/py/Locales/TL7.m3u8
 
 exit 0
