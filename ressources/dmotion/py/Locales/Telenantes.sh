e18524e7bb43165b39d2a55cdafa2f401ecf8789
Sibprod <109020245+Sibprod@users.noreply.github.com>
2023-10-17 13:54:09 +0200

    Update Telenantes.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Locales/Telenantes.sh
+++ ressources/dmotion/py/Locales/Telenantes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ressources/dmotion/py/Telenantes/Telenantes.py > ressources/dmotion/py/Telenantes/Telenantes.m3u8
+python3 ressources/dmotion/py/Locales/Telenantes.py > ressources/dmotion/py/Locales/Telenantes.m3u8
 
 exit 0
