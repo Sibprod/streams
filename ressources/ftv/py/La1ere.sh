5f1351ab855706412c6bef27e66f88d3112ca7ed
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-01-20 17:45:00 +0100

    Update La1ere.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/ftv/py/La1ere.sh
+++ ressources/ftv/py/La1ere.sh
@@ -13,14 +13,14 @@ if [ ! -d "$output_dir" ]; then
     exit 1
 fi
 
-1ereGuadeloupe_content=$(cat "$input_file" | sed 's/France_2/1erGuadeloupe/g; s/hls_fr2/hls_guadeloupe/g')
-1ereMartinique_content=$(cat "$input_file" | sed 's/France_2/1erMartinique/g; s/hls_fr2/hls_martinique/g')
-1ereGuyane_content=$(cat "$input_file" | sed 's/France_2/1erGuyane/g; s/hls_fr2/hls_guyane/g')
-1ereReunion_content=$(cat "$input_file" | sed 's/France_2/1erReunion/g; s/hls_fr2/hls_reunion/g')
-1ereMayotte_content=$(cat "$input_file" | sed 's/France_2/1erMayotte/g; s/hls_fr2/hls_mayotte/g')
+1GP_content=$(cat "$input_file" | sed 's/France_2/1erGuadeloupe/g; s/hls_fr2/hls_monde/g')
+1MQ_content=$(cat "$input_file" | sed 's/France_2/1erMartinique/g; s/hls_fr2/hls_monde/g')
+1GF=$(cat "$input_file" | sed 's/France_2/1erGuyane/g; s/hls_fr2/hls_monde/g')
+1RE_content=$(cat "$input_file" | sed 's/France_2/1erReunion/g; s/hls_fr2/hls_monde/g')
+1YT_content=$(cat "$input_file" | sed 's/France_2/1erMayotte/g; s/hls_fr2/hls_monde/g')
 
-echo "$1ereGuadeloupe_content" > "$output_dir/1ereGP.m3u8"
-echo "$1ereMartinique_content" > "$output_dir/1ereMQ.m3u8"
-echo "$1ereGuyane_content" > "$output_dir/1ereGF.m3u8"
-echo "$1ereReunion_content" > "$output_dir/1ereRE.m3u8"
-echo "$1ereMayotte_content" > "$output_dir/1ereYT.m3u8"
+echo "$ 1GP_content" > "$output_dir/1ereGP.m3u8"
+echo "$ 1MQ_content" > "$output_dir/1ereMQ.m3u8"
+echo "$ 1GF_content" > "$output_dir/1ereGF.m3u8"
+echo "$ 1RE_content" > "$output_dir/1ereRE.m3u8"
+echo "$ 1YT_content" > "$output_dir/1ereYT.m3u8"
