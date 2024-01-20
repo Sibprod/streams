84fdf2b8d2639c93e2c719b24f6d8840c6536cc5
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-01-20 13:26:38 +0100

    Update and rename LCP100_100.sh to LCP.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

---
+++ ressources/dmotion/py/Assemblees/LCP.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ressources/dmotion/py/Assemblees/LCP.py > ressources/dmotion/py/Assemblees/LCP.m3u8
+
+exit 0
--- ressources/dmotion/py/Assemblees/LCP100_100.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/dmotion/py/Assemblees/LCP100_100.py > ressources/dmotion/py/Assemblees/LCP100_100.m3u8
-
-exit 0
