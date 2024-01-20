1989c66d4818ea64355f24caed9290c5c59451ab
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-01-20 17:24:14 +0100

    Update La1ere.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/ftv/py/La1ere.py
+++ ressources/ftv/py/La1ere.py
@@ -6,7 +6,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:5')
 print('#EXT-X-STREAM-INF:BANDWIDTH=3032655,AVERAGE-BANDWIDTH=2756959,CODECS="avc1.64001f,mp4a.40.2",RESOLUTION=1280x720,FRAME-RATE=25.000,AUDIO="audio-AACL-96",SUBTITLES="text"')
 s = requests.Session()
-response = s.get(f'https://simulcast-p.ftven.fr/simulcast/1ereReunion/hls_reunion/index.m3u8')
+response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://simulcast-p.ftven.fr/simulcast/1ereReunion/hls_reunion/index.m3u8')
 
 string = response.text
 new_string = string.replace("index", "France_2-avc1_2500000=10001")
