9434a1a45435de228fda44b875766fcf5bfcfe37
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-01-20 13:36:55 +0100

    Update and rename Equipe.sh to LEquipe.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Equipe/Equipe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 ressources/dmotion/py/Equipe/Equipe.py > ressources/dmotion/py/Equipe/Equipe.m3u8
-
-exit 0
---
+++ ressources/dmotion/py/LEquipe/LEquipe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ressources/dmotion/py/LEquipe/LEquipe.py > ressources/dmotion/py/LEquipe/LEquipe.m3u8
+
+exit 0
