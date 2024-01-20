6bcf444ca5ca2941d79d64cd9cb4b36738ea00ee
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-01-20 15:50:50 +0100

    Update LEquipe.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/LEquipe/LEquipe.py
+++ ressources/dmotion/py/LEquipe/LEquipe.py
@@ -4,7 +4,7 @@ import requests
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
-    "Referer": "http://www.callofliberty.fr/"
+    "Referer": "http://s2.callofliberty.fr/"
 }
 
 print('#EXTM3U')
