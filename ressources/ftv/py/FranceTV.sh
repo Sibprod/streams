da6a073b9f6b869dd969980d0a0ad4b21f143f14
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-01-21 11:10:54 +0100

    Update FranceTV.sh
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/ftv/py/FranceTV.sh
+++ ressources/ftv/py/FranceTV.sh
@@ -18,13 +18,23 @@ fr4_content=$(cat "$input_file" | sed 's/France_2/France_4/g; s/hls_fr2/hls_fr4/
 fr5_content=$(cat "$input_file" | sed 's/France_2/France_5/g; s/hls_fr2/hls_fr5/g')
 noa_content=$(cat "$input_file" | sed 's/France_2/Regions_noa/g; s/hls_fr2/hls_noa/g')
 fri_content=$(cat "$input_file" | sed 's/France_2/France_Info/g; s/hls_fr2/hls_monde_frinfo/g')
+gua_metro_content=$(cat "$input_file" | sed 's/France_2/1er_Guadeloupe/g; s/hls_fr2/hls_monde_guadeloupe/g')
+mar_metro_content=$(cat "$input_file" | sed 's/France_2/1er_Martinique/g; s/hls_fr2/hls_monde_martinique/g')
+guy_metro_content=$(cat "$input_file" | sed 's/France_2/1er_Guyane/g; s/hls_fr2/hls_monde_guyane/g')
+may_metro_content=$(cat "$input_file" | sed 's/France_2/1er_Mayotte/g; s/hls_fr2/hls_monde_mayotte/g')
 reu_metro_content=$(cat "$input_file" | sed 's/France_2/1er_Reunion/g; s/hls_fr2/hls_monde_reunion/g')
-reu_content=$(cat "$input_file" | sed 's/France_2/1er_Reunion/g; s/hls_fr2/hls_reunion/g')
+nca_metro_content=$(cat "$input_file" | sed 's/France_2/1er_NC/g; s/hls_fr2/hls_monde_NC/g')
+
 
 echo "$fr3_content" > "$output_dir/France3.m3u8"
 echo "$fr4_content" > "$output_dir/France4.m3u8"
 echo "$fr5_content" > "$output_dir/France5.m3u8"
 echo "$noa_content" > "$output_dir/France3_NoA.m3u8"
 echo "$fri_content" > "$output_dir/FranceInfo.m3u8"
+echo "$gua_metro_content" > "$output_dir/La1ere_guadeloupe_metro.m3u8"
+echo "$mar_metro_content" > "$output_dir/La1ere_martinique_metro.m3u8"
+echo "$gua_metro_content" > "$output_dir/La1ere_guyane_metro.m3u8"
+echo "$may_metro_content" > "$output_dir/La1ere_mayotte_metro.m3u8"
 echo "$reu_metro_content" > "$output_dir/La1ere_reunion_metro.m3u8"
-echo "$reu_content" > "$output_dir/La1ere_reunion.m3u8"
+echo "$nca_metro_content" > "$output_dir/La1ere_NC_metro.m3u8"
+
