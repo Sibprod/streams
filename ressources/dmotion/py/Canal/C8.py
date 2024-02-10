a66e4d9126fd78762f3e1554164ca9c338137a63
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-02-10 23:01:03 +0100

    Update C8.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dmotion/py/Canal/C8.py
+++ ressources/dmotion/py/Canal/C8.py
@@ -39,7 +39,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
-with open('ressources/dmotion/py/Canal/C8_info.txt') as f:
+with open('ressources/dmotion/py/c8/c8_info.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
@@ -52,6 +52,10 @@ with open('ressources/dmotion/py/Canal/C8_info.txt') as f:
             tvg_id = line[3].strip()
         else:
             grab(line)
+            
+if 'temp.txt' in os.listdir():
+    os.system('rm temp.txt')
+    os.system('rm watch*')
             
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
