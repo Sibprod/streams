58a9b193215e7fbb9e0af1484e9d2d543e42f964
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-04-26 19:28:23 +0200

    Update FranceTVDocs.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/ftv/py/FranceTVDocs.py
+++ ressources/ftv/py/FranceTVDocs.py
@@ -6,7 +6,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5897200,CODECS="avc1.640028, mp4a.40.2", RESOLUTION=1920x1080, SUBTITLES="subs", AUDIO="audio_1000017564_128"')
 s = requests.Session()
-response = s.get(f'https://live-thema.ftven.fr/ZXhwPTE3MTQxNzM1NDN+YWNsPSUyZip+aG1hYz1kY2FkZjEyYmVhZTVlNDlhNGNkNTgwZDBmMWM4MGIyZDlmOTMxNWJlNGQyNTA4NTQ3N2YyMjVhODk4NGI1ZTU0/docs/735e9260-bb63-11ee-a1a7-0200170265fd_0_HLS-francedomtom/manifest.m3u8')
+response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://live-thema.ftven.fr/ZXhwPTE3MTQxNzM1NDN+YWNsPSUyZip+aG1hYz1kY2FkZjEyYmVhZTVlNDlhNGNkNTgwZDBmMWM4MGIyZDlmOTMxNWJlNGQyNTA4NTQ3N2YyMjVhODk4NGI1ZTU0/docs/735e9260-bb63-11ee-a1a7-0200170265fd_0_HLS-francedomtom/manifest.m3u8')
 
 string = response.text
 new_string = string.replace("manifest", "video_10801920_p_0.m3u8")
