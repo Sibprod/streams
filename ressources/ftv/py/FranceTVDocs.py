852779f6e3fd9681985dbba4d3536ea3b8c95d60
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-04-26 19:24:25 +0200

    Add files via upload
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

---
+++ ressources/ftv/py/FranceTVDocs.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import requests
+
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5897200,CODECS="avc1.640028, mp4a.40.2", RESOLUTION=1920x1080, SUBTITLES="subs", AUDIO="audio_1000017564_128"')
+s = requests.Session()
+response = s.get(f'https://live-thema.ftven.fr/ZXhwPTE3MTQxNzM1NDN+YWNsPSUyZip+aG1hYz1kY2FkZjEyYmVhZTVlNDlhNGNkNTgwZDBmMWM4MGIyZDlmOTMxNWJlNGQyNTA4NTQ3N2YyMjVhODk4NGI1ZTU0/docs/735e9260-bb63-11ee-a1a7-0200170265fd_0_HLS-francedomtom/manifest.m3u8')
+
+string = response.text
+new_string = string.replace("manifest", "video_10801920_p_0.m3u8")
+print(new_string)
+new2_string = string.replace("manifest", "A_audio_1000017564_128_fr")
+print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_1000017564_128",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="A_audio_1000017564_128_fr.m3u8",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="{new2_string}"')
---
+++ ressources/ftv/py/FranceTVDocs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 ressources/ftv/py/FranceTVSeries.py > ressources/ftv/py/FranceTVSeries.m3u8
+
+exit 0
