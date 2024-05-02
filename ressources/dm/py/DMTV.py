2e940fabea5c712f603c8ab93bd0907e96b8619e
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-05-02 15:41:38 +0200

    Update DMTV.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dm/py/DMTV.py
+++ ressources/dm/py/DMTV.py
@@ -4,7 +4,7 @@ import os
 def snif(line):
     try:
         idvideo = line.split('/')[-1]
-        url = (f'https://www.dailymotion.com/player/metadata/video/{idvideo}', proxies=proxies).json()['qualities']['auto'][0]['url']
+        url = (f'https://www.dailymotion.com/player/metadata/video/{idvideo}', proxies=proxies)
         response = requests.get(url).json()
         stream_url = response['qualities']['auto'][0]['url']
         m3u = requests.get(stream_url).text
