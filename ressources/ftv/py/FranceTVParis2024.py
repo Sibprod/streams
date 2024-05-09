1459ca3e06bd7a4597a12255133608e795740dde
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-05-09 10:18:27 +0200

    Update FranceTVParis2024.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/ftv/py/FranceTVParis2024.py
+++ ressources/ftv/py/FranceTVParis2024.py
@@ -4,12 +4,11 @@ import requests
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=5897200,CODECS="avc1.640028, mp4a.40.2", RESOLUTION=1920x1080, SUBTITLES="subs", AUDIO="audio_1000017564_128"')
+print('#EXT-X-STREAM-INF:BANDWIDTH=5605600,AVERAGE-BANDWIDTH=4918939,RESOLUTION=1920x1080,FRAME-RATE=25.000,CODECS="avc1.640028,mp4a.40.2"')
 s = requests.Session()
 response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://live-olympics.ftven.fr/paris2024-francedomtom/master.m3u8')
 
 string = response.text
-new_string = string.replace("master", "b0EF_720p", "b0EF_1080p")
+new_string = string.replace("playlist", "b0EF_720p", "b0EF_1080p")
 print(new_string)
-new2_string = string.replace("master", "b0EF_720p", "b0EF_1080p")
-print(f'#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="audio_1000017564_128",LANGUAGE="fr",NAME="fr",DEFAULT=YES,AUTOSELECT=YES,URI="{new2_string}"')
+
