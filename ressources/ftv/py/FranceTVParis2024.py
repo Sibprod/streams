ef3922ef19501501fd881d647c0b9f7b4e43d3a6
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-05-09 10:43:06 +0200

    Update FranceTVParis2024.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/ftv/py/FranceTVParis2024.py
+++ ressources/ftv/py/FranceTVParis2024.py
@@ -4,7 +4,7 @@ import requests
 
 print('#EXTM3U')
 print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:BANDWIDTH=5605600,AVERAGE-BANDWIDTH=4918939,RESOLUTION=1920x1080,FRAME-RATE=25.000,CODECS="avc1.640028,mp4a.40.2"')
+print('#EXT-X-STREAM-INF:BANDWIDTH=2855600,AVERAGE-BANDWIDTH=2479236,RESOLUTION=1280x720,FRAME-RATE=25.000,CODECS="avc1.640020,mp4a.40.2"')
 s = requests.Session()
 response = s.get(f'https://hdfauth.ftven.fr/esi/TA?url=https://live-olympics.ftven.fr/paris2024-francedomtom/master.m3u8')
 
@@ -12,6 +12,6 @@ string = response.text
 new_string = string.replace("playlist", "b0EF_720p")
 print(new_string)
 new2_string = string.replace("iframe", "b0EF_720p")
-print(f'#EXT-X-I-FRAME-STREAM-INF:BANDWIDTH=1150600,RESOLUTION=640x360,CODECS="avc1.4d401e", URI="{new2_string}"')
+print(f'#EXT-X-I-FRAME-STREAM-INF:BANDWIDTH=2855600,RESOLUTION=1280x720,CODECS="avc1.640020", URI="{new2_string}"')
 
 
