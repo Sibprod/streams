5fb2e615a2403d0a3154b5ea56c987f80d6e33ba
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-06-03 08:34:53 +0200

    Update Lequipe.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dm/py/dmfr/Lequipe.py
+++ ressources/dm/py/dmfr/Lequipe.py
@@ -6,7 +6,3 @@ s = requests.Session()
 response = s.get('https://pwultrp.alwaysdata.net/equipe.php?m3u8')
 
 print(response.text)
-    except Exception as e:
-        m3u = na
-    finally:
-        print(m3u)
