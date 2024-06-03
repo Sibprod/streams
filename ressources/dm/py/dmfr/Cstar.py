7994d8e91927613e69c3fecd6149b7788e353de5
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-06-03 09:03:34 +0200

    Update Cstar.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

--- ressources/dm/py/dmfr/Cstar.py
+++ ressources/dm/py/dmfr/Cstar.py
@@ -3,9 +3,25 @@
 import requests
 
 s = requests.Session()
-response = s.get('https://www.dai.ly/x5gv5v0')
+idvideo = line.split('/')[-1]
+        url = f'https://www.dailymotion.com/player/metadata/video/x5gv5v0'
+response = requests.get(url).json()
+        stream_url = response['qualities']['auto'][0]['url']
+        m3u = requests.get(stream_url).text
+return m3u
 
-print(response.text)
+output_directory = 'ressources/dm/py/hls'
+os.makedirs(output_directory, exist_ok=True)
 
+continue
+        elif line.startswith('#'):
+            current_category = line[1:]
+        elif line.startswith('https://'):
+            if current_category:
+                file_name = f"{current_category.strip()}.m3u8"
+                file_path = os.path.join(output_directory, file_name)
+                m3u_content = snif(line)
+                with open(file_path, 'w') as file:
+                    file.write(m3u_content)
 
 
