212fb0a2da3aff67c5b59e0cc2d5b828a0208348
Sibprod <109020245+Sibprod@users.noreply.github.com>
2024-10-18 12:12:52 +0200

    Update and rename Lequipe.py to LEquipe.py
    Signed-off-by: Sibprod <109020245+Sibprod@users.noreply.github.com>

---
+++ ressources/dm/py/dmfr/LEquipe.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+import requests
+
+s = requests.Session()
+idvideo = line.split('/')[-1]
+        url = f'https://tvradiozap.eu/tools/dm-m3u8.php/x2lefik.m3u8'
+response = requests.get(url).json()
+        stream_url = response['qualities']['auto'][0]['url']
+        m3u = requests.get(stream_url).text
+return m3u
+
+output_directory = 'ressources/dm/py/hls'
+os.makedirs(output_directory, exist_ok=True)
+
+continue
+        elif line.startswith('#'):
+            current_category = line[1:]
+        elif line.startswith('https://'):
+            if current_category:
+                file_name = f"{current_category.strip()}.m3u8"
+                file_path = os.path.join(output_directory, file_name)
+                m3u_content = snif(line)
+                with open(file_path, 'w') as file:
+                    file.write(m3u_content)
+
--- ressources/dm/py/dmfr/Lequipe.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python3
-
-import requests
-
-s = requests.Session()
-response = s.get('https://www.dailymotion.com/video/x2lefik')
-
-print(response.text)
